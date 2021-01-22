3504f1afc0201ce651b424fde1d4e51821876e9b
diem98 <13152410380@163.com>
2021-01-22 18:56:15 +0800

    esbuild -0.8

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const ESBuildWebpackPlugin = require('./esbuild-webpack-plugin/dist/index').default;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  "mode": "production",
+  entry: {
+    'app': './src/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+        type: 'javascript/auto'
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new ESBuildWebpackPlugin({})
+    ],
+    runtimeChunk: {
+      name: entrypoint => `runtime~${entrypoint.name}`
+    }
+    
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html'
+    })
+  ]
+}
